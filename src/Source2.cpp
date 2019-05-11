c91cd99c33ff6a18193e07f9e7e196f1af22b689
marlanader <38626216+marlanader@users.noreply.github.com>
2019-05-12 00:14:50 +0200

    Add files via upload

--- src/Source2.cpp
+++ src/Source2.cpp
@@ -6,159 +6,146 @@ using namespace std;
 
 struct  PINS
 {
-    string name;
-    string pin_state;
-    int firstcoordinate;
-    int secondcoordinate;
+	string name;
+	string pin_state;
+	int firstcoordinate;
+	int secondcoordinate;
 };
 vector <PINS> pins;
 struct coor
 {
-    string x,y;
+	string x, y;
 };
 
-struct conn{
-    string layer;
-    vector<coor> cord;
+struct conn {
+	string layer;
+	vector<coor> cord;
 
 };
 struct NETS
 {
-    string name;
-    vector<conn> connection;
-    vector <string> vias;
+	string name;
+	vector<conn> connection;
+	vector <string> vias;
 
 };
 NETS nets[5000];
 vector <string> number_c;
 void readfromDEF()
 {
-    bool pinsfound = false,netsfound=false,finish2=false;
-    ifstream DEF;
-    string word;
-    DEF.open("D://College//Semester 10-- Spring 2019//CSCE3304 - Digital Design II//Assignments//pro2//untitled//cpu.def");
+	bool pinsfound = false, netsfound = false, finish2 = false;
+	ifstream DEF;
+	string word;
+	DEF.open("cpu.def");
 
-        if (DEF.is_open())
-        {
+	if (DEF.is_open())
+	{
 
-            while (!DEF.eof())
-            {
-                DEF >> word;
-                if (word == "PINS")
-                {
-                    int index = 0;
+		while (!DEF.eof())
+		{
+			DEF >> word;
+			if (word == "PINS")
+			{
+				int index = 0;
 
-                    while (pinsfound == false)
-                    {
-                        DEF >> word;
-                        if (word == "-")
-                        {
+				while (pinsfound == false)
+				{
+					DEF >> word;
+					if (word == "-")
+					{
 
-                            pins.push_back({ " "," ",0,0 });
-                            DEF >> word;
-                            pins[index].name = word;
-                        }
-                        if ((word == "PLACED") || (word == "FIXED"))
-                        {
-                            pins[index].pin_state = word;
-                            DEF >> word;
-                            DEF >> word;
-                            pins[index].firstcoordinate = stoi(word);
+						pins.push_back({ " "," ",0,0 });
+						DEF >> word;
+						pins[index].name = word;
+					}
+					if ((word == "PLACED") || (word == "FIXED"))
+					{
+						pins[index].pin_state = word;
+						DEF >> word;
+						DEF >> word;
+						pins[index].firstcoordinate = stoi(word);
 
-                            DEF >> word;
-                            pins[index].secondcoordinate = stoi(word);
-                            index++;
-                        }
+						DEF >> word;
+						pins[index].secondcoordinate = stoi(word);
+						index++;
+					}
 
-                        if (word == "END")
-                            pinsfound = true;
+					if (word == "END")
+						pinsfound = true;
 
-                    }
-                }
-                if (word == "NETS")
-                {
-                    int index2 = 0;
-                    int metals_size = 0, firstc = 0;
-                    conn c;
-                while (netsfound == false)
-                {
-                    DEF >> word;
-                    if (word == "-")
-                    {
-                        DEF >> word;
-                        nets[index2].name = word;
+				}
+			}
+			if (word == "NETS")
+			{
+				int index2 = 0;
+				int metals_size = 0, firstc = 0;
+				conn c;
+				coor r;
+				int j = 0, k = 0;
+				DEF >> word;
+				DEF >> word;
+						while(word !="END")
+						{
+							DEF >> word;
+							if (word == "-")
+							{
+								DEF >> word;
+								nets[index2].name = word;
+								j = 0;
+							}
+							if ((word == "ROUTED") || (word == "NEW"))
+							{
+								DEF >> word;
+								c.layer = word;
+								//nets[index2].connection[i].layer=word;
+							}
+							if (word == "(")
+							{
+								DEF >> word;
+								r.x = word;
+								// nets[index2].connection[i].cord[j].x=word;
+								firstc++;
+								DEF >> word;
+								r.y = word;
+								// nets[index2].connection[i].cord[j].y=word;
+								j++;
+								c.cord.push_back(r);
 
-                    }
-                    if ((word == "ROUTED") || (word=="NEW") )
-                    {
-                        int i=0;
-                        DEF >> word;
-                        c.layer=word;
-                        //nets[index2].connection[i].layer=word;
+							}
+							if (word == ")")
+							{
+								DEF >> word;
+								if ((word != "NEW") && (word != "("))
+								{
 
-                        finish2 = false;
-                        int j=0,k=0;
-                        coor r;
-                        do
-                        {
-                            DEF >> word;
-                            if (word == "(")
-                            {
-                                DEF >> word;
-                                r.x=word;
-                               // nets[index2].connection[i].cord[j].x=word;
-                                firstc++;
-                                DEF >> word;
-                                r.y=word;
-                               // nets[index2].connection[i].cord[j].y=word;
-                                j++;
-                                c.cord.push_back(r);
+									nets[index2].vias.push_back(word);
+									if (index2 == 1)   cout << nets[index2].vias.back() << endl;
+									k++;
+								}
 
-                            }
-                            if (word == ")")
-                            {
-                                DEF >> word;
-                                if ((word != "NEW") && (word !="(") )
-                                {
+							}
+							if (word == ";")
+								index2++;
 
-                                    nets[index2].vias.push_back(word);
-                                if (index2==1)   cout << nets[index2].vias.back() << endl;
-                                    k++;
-                                }
+						}
+	
+						nets[index2].connection.push_back(c);
+					
+			}
 
+		}
 
-                            if (word == "NEW")
-                                finish2 = true;
-}
-                            if (word == "NEW")
-                                finish2 = true;
-                        } while (finish2 == false);
-                        nets[index2].connection.push_back(c);
-
-                        i++;
-}
-
-                    if (word == ";")
-                        index2++;
-
-                    if (word == "END")
-                        netsfound = true;
-
-                }
-            }
-
-            }
-
-        }
-        else
-        {
-            cout << "cant open file" << endl;
-        }
+	}
+	else
+	{
+		cout << "cant open file" << endl;
+	}
 
 
 }
 int main()
 {
-    readfromDEF();
-    return 0;
+	readfromDEF();
+	system("PAUSE");
+	return 0;
 }
