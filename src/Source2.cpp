c83329f61350ecb30a45ce05b7a3c7cff31e4ee2
shereenafify <33997124+shereenafify@users.noreply.github.com>
2019-05-12 01:02:57 +0200

    Update Source2.cpp

--- src/Source2.cpp
+++ src/Source2.cpp
@@ -1,9 +1,11 @@
-#include <string>
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
 using namespace std;
-
+int nets_size=0; 
 struct  PINS
 {
 	string name;
@@ -12,6 +14,29 @@ struct  PINS
 	int secondcoordinate;
 };
 vector <PINS> pins;
+
+
+
+struct Layer {
+	string name; //Layer Name
+	string resistance_unit; //Layer resistance unit (RPERSQ Mostly)
+	string resistance_value; //Layer resistance value
+	string capacitance_unit; //Layer Capacitance unit (CPERSQDIST Mostly)
+	string capacitance_value; //Layer Capacitance value
+	string edge_capacitance; //Layer edge capacitance 
+	string width;
+	string spacing;
+
+};
+vector < Layer > layers; //those are the layers of TYPE ROUTING only
+vector < string > all_layers; //those are all the layers in a file
+
+struct VIA {
+	string name; //the VIA Name
+	string resistance; //the VIA resistance
+};
+vector < VIA > via; //ALL the VIAS
+
 struct coor
 {
 	string x, y;
@@ -30,16 +55,18 @@ struct NETS
 
 };
 NETS nets[5000];
-vector <string> number_c;
+
 void readfromDEF()
 {
 	bool pinsfound = false, netsfound = false, finish2 = false;
 	ifstream DEF;
 	string word;
+	int nets_size=0;
 	DEF.open("cpu.def");
 
 	if (DEF.is_open())
 	{
+
 
 		while (!DEF.eof())
 		{
@@ -84,53 +111,56 @@ void readfromDEF()
 				int j = 0, k = 0;
 				DEF >> word;
 				DEF >> word;
-						while(word !="END")
+				while (word != "END")
+				{
+					DEF >> word;
+					if (word == "-")
+					{
+						DEF >> word;
+						nets[index2].name = word;
+						j = 0;
+						nets_size++;
+					}
+					if ((word == "ROUTED") || (word == "NEW"))
+					{
+						DEF >> word;
+						c.layer = word;
+						//nets[index2].connection[i].layer=word;
+					}
+					if (word == "(")
+					{
+						DEF >> word;
+						r.x = word;
+						// nets[index2].connection[i].cord[j].x=word;
+						firstc++;
+						DEF >> word;
+						r.y = word;
+						// nets[index2].connection[i].cord[j].y=word;
+						j++;
+						c.cord.push_back(r);
+
+					}
+					if (word == ")")
+					{
+						DEF >> word;
+						if ((word != "NEW") && (word != "(") && (word !=";") && (word!="+"))
 						{
-							DEF >> word;
-							if (word == "-")
-							{
-								DEF >> word;
-								nets[index2].name = word;
-								j = 0;
-							}
-							if ((word == "ROUTED") || (word == "NEW"))
-							{
-								DEF >> word;
-								c.layer = word;
-								//nets[index2].connection[i].layer=word;
-							}
-							if (word == "(")
-							{
-								DEF >> word;
-								r.x = word;
-								// nets[index2].connection[i].cord[j].x=word;
-								firstc++;
-								DEF >> word;
-								r.y = word;
-								// nets[index2].connection[i].cord[j].y=word;
-								j++;
-								c.cord.push_back(r);
-
-							}
-							if (word == ")")
-							{
-								DEF >> word;
-								if ((word != "NEW") && (word != "("))
-								{
-
-									nets[index2].vias.push_back(word);
-									if (index2 == 1)   cout << nets[index2].vias.back() << endl;
-									k++;
-								}
-
-							}
-							if (word == ";")
-								index2++;
-
+							
+							nets[index2].vias.push_back(word);
+							
+							k++;
 						}
-	
-						nets[index2].connection.push_back(c);
-					
+
+					}
+					if (word == ";")
+					{
+						
+						index2++;
+					}
+				}
+
+				nets[index2].connection.push_back(c);
+
 			}
 
 		}
@@ -143,9 +173,122 @@ void readfromDEF()
 
 
 }
-int main()
-{
-	readfromDEF();
+
+void readfromLEF() {
+	string x, x1, x2, x3, x4, width_l, name,space;
+	bool done = false;
+	int count = 0;
+	ifstream LEF;
+	LEF.open("tmp.lef");
+	if (LEF.is_open()) {
+		while (!LEF.eof()) {
+			LEF >> x;
+			if (x == "LAYER") {
+
+				LEF >> name;
+				all_layers.push_back(name);
+				sort(all_layers.begin(), all_layers.end());
+				all_layers.erase(unique(all_layers.begin(), all_layers.end()), all_layers.end());
+				LEF >> x;
+				LEF >> x;
+
+				if (x == "ROUTING") {
+					done = false;
+					do {
+						LEF >> x;
+						if (x == "WIDTH")
+						{
+							LEF >> width_l;
+
+						}
+						if (x == "SPACING")
+						{
+							LEF >> space;
+						}
+						if (x == "RESISTANCE") {
+							LEF >> x1; //units	
+							LEF >> x2; //value		
+						}
+						if (x == "CAPACITANCE") {
+							LEF >> x3;
+							LEF >> x4;
+							count++;
+						}
+						if (x == "EDGECAPACITANCE") {
+							LEF >> x;
+							layers.push_back({name,x1,x2,x3,x4,x,width_l,space});
+							done = true;
+						}
+						
+					} while (done == false);
+				}
+			}
+			if (x == "VIA") {
+
+				string res, name2;
+				bool flag = false;
+				LEF >> name2;
+				do {
+					LEF >> x;
+					if (x == "RESISTANCE") {
+						LEF >> res;
+						via.push_back({name2,res});
+						flag = true;
+					}
+				} while (flag == false);
+			}
+		}
+	}
+	else {
+		cout << "unable to open LEF file" << endl;
+	}
+	LEF.close();
+}
+float calculateSegmentLength(int first_coordinate_x,int second_coordinate_x,int first_coordinate_y,int second_coordinate_y){
+	return	sqrt(pow(first_coordinate_x+second_coordinate_x,2)+pow(first_coordinate_y+second_coordinate_y,2));
+}
+float calculateSegmentCapacitance(float width, float spacing, float capacitance, float edge_capacitance) { //area*CPERSQDIST+length*EDGECAP from LEF
+	return (width*spacing*capacitance) + (spacing*edge_capacitance); // (WIDTH*SPACING*CPERSQDIST)+(SPACING*EDGECAP)
+}
+float calculateSheetResistance(float sheet_resistance, float length, float width) { //per square //taken from LEF
+	return sheet_resistance * (length / width); //parasitic_resistance
+}
+float calculateViaResistance(float via_resistance) {
+	return via_resistance;
+}
+int main() {
+	
+	int choice;
+	do {
+		cout << "Which file would you like to enter? 1:DEF 2:LEF" << endl;
+		cin >> choice;
+	} while (choice < 1 || choice > 2);
+	if (choice == 1)
+		readfromDEF();
+	else if (choice == 2)
+		readfromLEF();
+		
+    cout << "*CAP" << endl;
+	for (int i = 0; i < layers.size(); i++) {
+		for (int j = 0; j < nets_size; j++) {
+			for (int k=0;k< nets[j].connection.size();k++){// per segment
+				cout << i + 1 << "  " << layers[i].name << "  " << calculateSegmentCapacitance(stof(layers[i].width),calculateSegmentLength(stoi(nets[i].connection[j].cord[j].x),stoi(nets[i].connection[j].cord[j].y),stoi(nets[i+1].connection[j].cord[j].x),stoi(nets[i+1].connection[j].cord[j].y)) , stof(layers[i].capacitance_value), stof(layers[i].edge_capacitance)) << endl;
+			}
+		}
+	}
+
+	cout << "*RES" << endl;
+	for (int i = 0; i < layers.size(); i++) { //per layers  
+		for (int j = 0; j < nets_size; j++) {//per net
+			for (int k=0;k< nets[j].connection.size();k++){// per segment
+				cout << i + 1 << "  " << layers[i].name << "  " << calculateSheetResistance(stof(layers[i].resistance_value), calculateSegmentLength(stoi(nets[i].connection[j].cord[j].x),stoi(nets[i].connection[j].cord[j].y),stoi(nets[i+1].connection[j].cord[j].x),stoi(nets[i+1].connection[j].cord[j].y)), stoi(layers[i].width)) << endl;
+			}
+		}
+	}
+
+	for (int i = 0; i < via.size(); i++) {
+		cout << i + 1 << "  " << via[i].name << "  " << calculateViaResistance(stof(via[i].resistance)) << endl;
+	}
 	system("PAUSE");
 	return 0;
 }
